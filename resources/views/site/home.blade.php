7c7a821bc503b08a9e0a721b8c44bbdd47ac0861
juliano <jot.asa@hotmail.com>
2023-12-29 19:23:30 -0300

    link_sneaker

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -15,27 +15,91 @@
             </div>
         </div>
         <div class="col-2">
-            <div class="border p-1">
+            <div class="border p-1 rounded-end mb-2">
                 <a href="#" class="btn btn-sm"><img src="https://cdn1.iconfinder.com/data/icons/shopping-223/33/shopping-04-256.png" alt="central de atendimento" width="20%"> Central de atendimento</a>
             </div>
         </div>
         <div class="col-2">
-            <div class="border p-1">
+            <div class="border p-1 rounded-end mb-2">
                 <a href="welcome" class="btn btn-sm"><img src="https://cdn0.iconfinder.com/data/icons/ui-essentials-2-3/32/user_interface_ui_basic_app_ui_interface_login_login-256.png" alt="login" width="20%"> Login</a>
             </div>
         </div>
         <div class="col-2">
-            <div class="border p-1">
+            <div class="border p-1 rounded-end mb-2">
                 <a href="#" class="btn btn-sm"><img src="https://cdn1.iconfinder.com/data/icons/material-core/20/shopping-cart-256.png" alt="carrinho" width="20%"> Meu carrinho</a>
             </div>
         </div>
     </div>
 </header>
 <div class="container-fluid">
-    <div class="row">
-        div.
+    <div class="row bg-dark g-4 justify-content-center gap-5">
+        <div class="col-1 p-1">
+            <a href="#" class="bg-dark link-light link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover">Vestuário</a>
+        </div>
+        <div class="col-1 p-1">
+            <a href="#" class="bg-dark link-light link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover">Calçados</a>
+        </div>
+        <div class="col-1 p-1">
+            <a href="#" class="bg-dark link-light link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover">Esportes</a>
+        </div>
+        <div class="col-1 p-1">
+            <a href="#" class="bg-dark link-light link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover">Relógios</a>
+        </div>
     </div>
 </div>
-<body>
-</body>
+<div class="container-sm w-75 mt-3">
+    <div class="card text-bg-dark w-100">
+        <img src="img/sneaker.png" class="card-img" alt="tenis">
+        <div class="card-img-overlay">
+            <div class="card" style="width: 20rem;">
+                <div class="card-body">
+                  <h5 class="card-title fw-bold">Corrida em alta performance</h5>
+                  <h6 class="card-subtitle mb-2 text-body-secondary">Nike</h6>
+                  <p class="card-text mt-5">As coisas ficam melhores quando se é um membro da Nike.Tenha acesso exclusivo em primeira mão aos novos lançamentos,inovações, frete grátis, benefícios especiais de aniversário e muito mais.</p>
+                  <a href="#" class="btn btn-primary mt-5 col-12" role="button">Confira</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-sm w-75 mt-2">
+    <div class="row g-4 justify-content-center gap-5">
+        <div class="col-2">
+            <img src="https://cdn3.iconfinder.com/data/icons/glypho-shopping-and-ecommerce/64/truck-fast-delivery-speed-256.png" alt="entrega" width="20%">
+             Entregamos em todo Brasil
+        </div>
+        <div class="col-2">
+            <img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-card-256.png" alt="cartão de crédito" width="15%">
+             Em até 12 vezes sem juros
+        </div>
+        <div class="col-2">
+            <img src="https://cdn4.iconfinder.com/data/icons/e-commerce-ui-outline/64/ecommerce_-_barcode-64.png" alt="boleto" width="15%">
+             Desconto de 10% no boleto
+        </div>
+        <div class="col-2">
+            <img src="https://cdn3.iconfinder.com/data/icons/approved-and-checked/85/approved_tick_shield_protection_protect_checked-512.png" alt="seguro" width="15%">
+             100% seguro            
+        </div>
+    </div>
+</div>
+<div class="container-sm w-25 mt-2">
+    <div class="row g-2">
+        <div class="card text-bg-dark col-6">
+            <img src="img/sneaker.png" class="card-img" alt="..." width="50%">
+            <div class="card-img-overlay">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small>Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+        <div class="card text-bg-dark col-6">
+            <img src="img/sneaker.png" class="card-img" alt="..." width="50%">
+            <div class="card-img-overlay">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small>Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+    </div>
+</div>
 </html>
