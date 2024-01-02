6627ac07a62b1aed1a771aed0b0a0680e1612a9f
juliano <jot.asa@hotmail.com>
2024-01-02 16:43:03 -0300

    register_done

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -10,8 +10,9 @@
 <header class="container-fluid border border-1 border-start-0 border-end-0 p-4">
     <div class="row g-2">
         <div class="col-6">
-            <div class="display-6">
-                Shoptime
+            <div class="display-6 mb-2">
+                <img src="https://cdn0.iconfinder.com/data/icons/e-commerce-53/512/eCommerce_BestSellers-256.png" alt="home" width="10%">
+                 Best Seller
             </div>
         </div>
         <div class="col-2">
@@ -21,7 +22,7 @@
         </div>
         <div class="col-2">
             <div class="border p-1 rounded-end mb-2">
-                <a href="welcome" class="btn btn-sm"><img src="https://cdn0.iconfinder.com/data/icons/ui-essentials-2-3/32/user_interface_ui_basic_app_ui_interface_login_login-256.png" alt="login" width="20%"> Login</a>
+                <a href="login" class="btn btn-sm"><img src="https://cdn0.iconfinder.com/data/icons/ui-essentials-2-3/32/user_interface_ui_basic_app_ui_interface_login_login-256.png" alt="login" width="20%"> Login</a>
             </div>
         </div>
         <div class="col-2">
@@ -47,16 +48,21 @@
         </div>
     </div>
 </div>
-<div class="container-sm w-75 mt-3">
+<div class="container-sm w-100 mt-3">
     <div class="card text-bg-dark w-100">
         <img src="img/sneaker.png" class="card-img" alt="tenis">
         <div class="card-img-overlay">
-            <div class="card" style="width: 20rem;">
-                <div class="card-body">
-                  <h5 class="card-title fw-bold">Corrida em alta performance</h5>
-                  <h6 class="card-subtitle mb-2 text-body-secondary">Nike</h6>
-                  <p class="card-text mt-5">As coisas ficam melhores quando se é um membro da Nike.Tenha acesso exclusivo em primeira mão aos novos lançamentos,inovações, frete grátis, benefícios especiais de aniversário e muito mais.</p>
-                  <a href="#" class="btn btn-primary mt-5 col-12" role="button">Confira</a>
+            <div class="container-sm">
+                <div class="row">
+                    <div class="card" style="width: 47%;">
+                        <div class="card-body">
+                        <h5 class="card-title fw-bold">Corrida em alta performance</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Nike</h6>
+                        <p class="card-text mt-5">As coisas ficam melhores quando se é um membro da Nike.Tenha acesso exclusivo em primeira mão aos novos lançamentos,inovações, frete grátis, benefícios especiais de aniversário e muito mais.
+                            A Nike representa uma cultura global de esporte, estilo e da busca pela grandeza. Somos apaixonados pelo potencial e acreditamos que todos deveriam ter as ferramentas para alcançar o potencial de cada um. Investimos em nossos funcionários, inspirando novas ideias, métodos e produtos que nos levam ao futuro do esporte.</p>
+                        <a href="#" class="btn btn-primary mt-5 col-12" role="button">Confira</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -82,9 +88,9 @@
         </div>
     </div>
 </div>
-<div class="container-sm w-25 mt-2">
-    <div class="row g-2">
-        <div class="card text-bg-dark col-6">
+<div class="container-sm mt-5 mb-5">
+    <div class="d-flex flex-row gap-5 g-2">
+        <div class="card text-bg-dark">
             <img src="img/sneaker.png" class="card-img" alt="..." width="50%">
             <div class="card-img-overlay">
               <h5 class="card-title">Card title</h5>
@@ -92,7 +98,7 @@
               <p class="card-text"><small>Last updated 3 mins ago</small></p>
             </div>
         </div>
-        <div class="card text-bg-dark col-6">
+        <div class="card text-bg-dark">
             <img src="img/sneaker.png" class="card-img" alt="..." width="50%">
             <div class="card-img-overlay">
               <h5 class="card-title">Card title</h5>
